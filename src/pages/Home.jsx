6b5d38bc9c6d1bad15ee00816dd154043a37f730
Roman.P <killme15139214@gmail.com>
2022-06-20 21:31:57 +0300

    feat: add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import ItemCard from "../components/ItemCard/ItemCard";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((arr) => setItems(arr));
+  }, []);
+  // const [searchValue, SetSearchValue] = useState("");
+  return (
+    <div className="homeWrapper">
+      {/* <Slider /> */}
+      <section className="productSection">
+        {items.map((arr) => (
+          <ItemCard key={arr.id} {...arr} />
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Home;
