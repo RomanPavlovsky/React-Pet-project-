fc7adb6c1886727499889b59a5d7e9ca2ae71f1c
Roman.P <killme15139214@gmail.com>
2022-06-20 20:27:00 +0300

    feat: add categories component

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import style from "categories.module.scss";
+
+const Categories = (active, setActive) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const categories = [
+    "Все",
+    "Мужская одежда",
+    "Женская одежда",
+    "Электроника",
+    "Украшения",
+  ];
+  const onClickCategory = (i) => setActiveIndex(i);
+  return (
+    <div className={style.wrapper}>
+      <div className={style.categories}>
+        <ul>
+          {categories.map((value, i) => (
+            <li
+              key={i}
+              onClick={() => onClickCategory(i)}
+              className={activeIndex === i ? style.active : ""}
+            >
+              {value}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
